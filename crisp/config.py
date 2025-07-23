4b81d820b783c119da220d9866b3d98df66e2975
Stuart Pernsteiner <spernsteiner@galois.com>
2025-07-23 10:08:35 -0700

    config: add transpile section

--- crisp/config.py
+++ crisp/config.py
@@ -1,24 +1,16 @@
 from dataclasses import dataclass
 import os
 import toml
+import typing
 
-@dataclass(frozen = True)
-class Config:
-    config_path: str
-    src_globs: list[str]
-    test_command: str
-    base_dir: str = '.'
-    mvir_storage_dir: str = 'crisp-storage'
-
-    def __post_init__(self):
-        config_dir = os.path.dirname(self.config_path)
-        object.__setattr__(self, 'base_dir', os.path.join(config_dir, self.base_dir))
-        object.__setattr__(self, 'mvir_storage_dir',
-            os.path.join(config_dir, self.mvir_storage_dir))
-
+class ConfigBase:
     @classmethod
     def from_dict(cls, d, config_path, **kwargs):
         d.update(kwargs)
+        field_tys = typing.get_type_hints(cls)
+        for k, v in d.items():
+            if issubclass(field_tys[k], ConfigBase):
+                d[k] = field_tys[k].from_dict(v, config_path)
         return cls(config_path=config_path, **d)
 
     @classmethod
@@ -30,3 +22,33 @@ class Config:
             path = f.name
         d = toml.load(f)
         return cls.from_dict(d, path, **kwargs)
+
+@dataclass(frozen = True)
+class Config(ConfigBase):
+    config_path: str
+
+    transpile: 'TranspileConfig'
+
+    src_globs: list[str]
+    test_command: str
+    base_dir: str = '.'
+    mvir_storage_dir: str = 'crisp-storage'
+
+    def __post_init__(self):
+        config_dir = os.path.dirname(self.config_path)
+        object.__setattr__(self, 'base_dir', os.path.join(config_dir, self.base_dir))
+        object.__setattr__(self, 'mvir_storage_dir',
+            os.path.join(config_dir, self.mvir_storage_dir))
+
+@dataclass(frozen = True)
+class TranspileConfig(ConfigBase):
+    config_path: str
+    cmake_src_dir: str
+    output_dir: str
+
+    def __post_init__(self):
+        config_dir = os.path.dirname(self.config_path)
+        object.__setattr__(self, 'cmake_src_dir',
+            os.path.join(config_dir, self.cmake_src_dir))
+        object.__setattr__(self, 'output_dir',
+            os.path.join(config_dir, self.output_dir))

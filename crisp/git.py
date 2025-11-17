b70e763d9e41f9ab82fc6b116d4bb67b11b2f12c
Khyber Sen <kkysen@gmail.com>
2025-11-17 08:22:47 -0800

    crisp: add type for `tree_files` with `TypeAlias` for the recursive type

--- crisp/git.py
+++ crisp/git.py
@@ -1,6 +1,6 @@
 import os
 import pygit2
-from typing import Optional
+from typing import Optional, TypeAlias
 
 from . import mvir as mvir_module
 from .mvir import MVIR, TreeNode, FileNode
@@ -76,6 +76,9 @@ def render(mvir: MVIR, target: TreeNode) -> pygit2.Oid:
     return commit
 
 
+Tree: TypeAlias = dict[str, "Tree" | FileNode]
+
+
 def commit_tree(
     mvir: MVIR,
     repo: pygit2.Repository,
@@ -86,7 +89,7 @@ def commit_tree(
     # Convert the `TreeNode`'s flat path->ID mapping to a nested structure like
     # the one used by git tree objects.  At each level, each entry is either a
     # `FileNode` or a dict representing a directory.
-    tree_files = {}
+    tree_files: Tree = {}
 
     def get_parent_and_name(path):
         head, tail = os.path.split(path)

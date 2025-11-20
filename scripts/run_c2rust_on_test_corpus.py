a8d0d47824c4f2ab402453a2ffc4a6df87b92b6b
Sourya Dey <sourya@galois.com>
2025-11-20 17:17:59 -0500

    Make run_c2rust() a function and use it for test corpus
    TODO edit for CRUST-Bench

--- scripts/run_c2rust_on_test_corpus.py
+++ scripts/run_c2rust_on_test_corpus.py
@@ -5,50 +5,67 @@ import subprocess
 from tqdm import tqdm
 
 
-TEST_CORPUS_PATH = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'Test-Corpus'
-CONVERTED_RUST_PATH = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'Test-Corpus-Converted-Rust'
-
-project_folder_paths = []
-project_folder_paths.extend(sorted([f for f in (TEST_CORPUS_PATH / 'Public-Tests/B01_synthetic').iterdir() if f.is_dir()]))
-project_folder_paths.extend(sorted([f for f in (TEST_CORPUS_PATH / 'Public-Tests/B01_organic').iterdir() if f.is_dir()]))
-
-for project_folder_path in tqdm(project_folder_paths):
+def run_c2rust(
+    c_project_folder: Path,
+    rust_project_folder: Path
+):
 
     # Build C project
-    c_project_folder_path = project_folder_path / 'test_case'
-    c_build_path = c_project_folder_path / 'build'
-    c_build_path.mkdir()
+    c_build_folder = c_project_folder / 'build'
+    c_build_folder.mkdir()
     subprocess.run(
         ['cmake', '-DCMAKE_EXPORT_COMPILE_COMMANDS=1', '..'],
-        cwd = c_build_path
+        cwd = c_build_folder
     )
-
-    # Run C2Rust
-    try:
-        subprocess.run(
-            ['c2rust', 'transpile', c_build_path / 'compile_commands.json'],
-            check = True,
-            capture_output = True
-        )
-    except subprocess.CalledProcessError as e:
-        with open('run_c2rust_on_test_corpus_failure_report.txt', 'a', encoding='utf-8') as f:
-            f.write(f'==========\nC2Rust failed on {project_folder_path.relative_to(TEST_CORPUS_PATH)}\n==========\n\nOutput: {e.output}\n\nError: {e.stderr}\n\n')
-    c2rust_output_file_paths = [f for f in (c_project_folder_path / 'src').iterdir() if f.suffix == '.rs']
 
     # Create Rust project
-    rust_project_folder_name = f'c2rust_{project_folder_path.stem}'
-    rust_project_folder_parent = CONVERTED_RUST_PATH / project_folder_path.relative_to(TEST_CORPUS_PATH)
-    rust_project_folder_parent.mkdir()
+    rust_project_folder.mkdir(parents = True)
     subprocess.run(
-        ['cargo', 'new', rust_project_folder_name],
-        cwd = rust_project_folder_parent
+        ['cargo', 'init'],
+        cwd = rust_project_folder
     )
-    rust_project_folder_path = rust_project_folder_parent / rust_project_folder_name
-    (rust_project_folder_path / 'src/main.rs').unlink()
+    (rust_project_folder / 'src/main.rs').unlink()
 
-    # Move C2Rust output files to Rust project
-    for c2rust_output_file_path in c2rust_output_file_paths:
-        c2rust_output_file_path.rename(rust_project_folder_path / 'src' / c2rust_output_file_path.name)
+    # Run C2Rust
+    subprocess.run(
+        ['c2rust', 'transpile', '-o', rust_project_folder, '--overwrite-existing', c_build_folder / 'compile_commands.json'],
+        check = True,
+        capture_output = True
+    )
 
     # Clear C build
-    shutil.rmtree(c_build_path)
+    shutil.rmtree(c_build_folder)
+
+
+def run_c2rust_on_test_corpus():
+    test_corpus_repo = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'Test-Corpus'
+
+    # Synthetic
+    c_project_folders = sorted([f / 'test_case' for f in (test_corpus_repo / 'Public-Tests/B01_synthetic').iterdir() if f.is_dir()])
+    for c_project_folder in tqdm(c_project_folders):
+        run_c2rust(
+            c_project_folder = c_project_folder,
+            rust_project_folder = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'converted_rust_projects/c2rust_Test-Corpus_B01_synthetic' / c_project_folder.parent.name[4:] # [4:] is to remove the leading 0NN_ since Rust project names cannot start with digits
+        )
+
+    # Organic
+    c_project_folders = sorted([f / 'test_case' for f in (test_corpus_repo / 'Public-Tests/B01_organic').iterdir() if f.is_dir()])
+    for c_project_folder in tqdm(c_project_folders):
+        run_c2rust(
+            c_project_folder = c_project_folder,
+            rust_project_folder = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'converted_rust_projects/c2rust_Test-Corpus_B01_organic' / c_project_folder.parent.name
+        )
+
+def run_c2rust_on_crust_bench():
+    crust_bench_repo = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'CRUST-bench'
+    c_project_folders = sorted([f for f in (crust_bench_repo / 'datasets/CBench').iterdir() if f.is_dir()])
+    for c_project_folder in tqdm(c_project_folders):
+        run_c2rust(
+            c_project_folder = c_project_folder,
+            rust_project_folder = Path(os.path.dirname(os.path.realpath(__file__))).resolve().parent.parent / 'converted_rust_projects/c2rust_CRUST-Bench' / c_project_folder.name
+        )
+
+
+if __name__ == "__main__":
+    run_c2rust_on_test_corpus()
+    run_c2rust_on_crust_bench()

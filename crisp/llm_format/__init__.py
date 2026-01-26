766ce5baa38dc9274802d4b1e10032d5c515a656
Stuart Pernsteiner <spernsteiner@galois.com>
2026-01-26 13:59:37 -0800

    llm: allow setting file formatter from config file or env var

--- crisp/llm_format/__init__.py
+++ crisp/llm_format/__init__.py
@@ -1,101 +1,14 @@
-import os
-import pathlib
-
-from ..mvir import MVIR, FileNode, TreeNode
-
 from .abc import LLMFileFormat
 from . import markdown
 from . import xml
 
-def _mode_to_format(mode: str) -> LLMFileFormat:
-    match mode:
-        case 'markdown':
-            return markdown.MarkdownFileFormat()
-        case 'xml':
-            return xml.XmlFileFormat()
-        case _:
-            raise ValueError(f'unknown mode {mode!r}')
+_FILE_FORMATTER_CLASSES_BY_NAME = {
+    'markdown': markdown.MarkdownFileFormat,
+    'xml': xml.XmlFileFormat,
+}
 
-
-def get_output_instructions(mode: str) -> str:
-    '''
-    Returns output formatting instructions for inclusion in the LLM prompt.
-    This will be something like:
-
-        Output the new code in foo format with bar delimiters.  Always do X;
-        never do Y.
-    '''
-    fmt = _mode_to_format(mode)
-    return fmt.get_output_instructions()
-
-def get_output_instructions_lowercase(mode: str) -> str:
-    '''
-    Like `get_output_instructions`, but the first letter is lowercased, so it
-    can be prefixed with an introductory clause.  Example usage:
-
-        'After finishing the task, ' + get_output_instructions_lowercase(mode)
-
-    This produces something like:
-
-        After finishing the task, output the new code in foo format with bar
-        delimiters.  Always do X; never do Y.
-    '''
-
-    s = get_output_instructions(mode)
-    if len(s) > 0:
-        s = s[0].lower() + s[1:]
-    return s
-
-def emit_file(
-    mode: str,
-    n: FileNode,
-    path: str,
-) -> (str, dict[str, str]):
-    fmt = _mode_to_format(mode)
-    return fmt.emit_file(n, path)
-
-def emit_files(
-    mvir: MVIR,
-    mode: str,
-    n: TreeNode,
-    glob_filter: str = None,
-) -> (str, dict[str, str]):
-    """
-    Generate formatted text giving the contents of files in `n`, along with a
-    dict mapping short path names used in the output to full paths as used in
-    `n`.  Output is formatted like `emit_file`.  If `glob_filter` is set to a
-    string, only files whose paths match that glob pattern will be included.
-    """
-    assert isinstance(n, TreeNode)
-
-    if isinstance(glob_filter, str):
-        glob_filter = (glob_filter,)
-
-    if len(n.files) == 0:
-        common_prefix = ''
-    elif len(n.files) == 1:
-        common_prefix = os.path.dirname(list(n.files.keys())[0])
-    else:
-        common_prefix = os.path.commonpath(n.files.keys())
-
-    parts = []
-    short_path_map = {}
-    for path, child_id in n.files.items():
-        if glob_filter is not None:
-            path_obj = pathlib.Path(path)
-            glob_match = any(path_obj.match(g) for g in glob_filter)
-            if not glob_match:
-                continue
-
-        short_path = os.path.relpath(path, common_prefix)
-        assert short_path not in short_path_map
-        short_path_map[short_path] = path
-
-        child_node = mvir.node(child_id)
-        part = emit_file(mode, child_node, short_path)
-        parts.append(part)
-    return '\n\n'.join(parts), short_path_map
-
-def extract_files(s: str, mode: str) -> list[tuple[str, str]]:
-    fmt = _mode_to_format(mode)
-    return fmt.extract_files(s)
+def get_file_formatter(name: str, **kwargs) -> LLMFileFormat:
+    cls = _FILE_FORMATTER_CLASSES_BY_NAME.get(name)
+    if cls is None:
+        raise ValueError(f'unknown file formatter {name!r}')
+    return cls(**kwargs)
--- crisp/workflow.py
+++ crisp/workflow.py
@@ -392,8 +392,7 @@ class Workflow:
     def llm_safety_op(self, n_code: TreeNode) -> tuple[TreeNode, LlmOpNode]:
         return llm.run_rewrite(
                 self.cfg, self.mvir, LLM_SAFETY_PROMPT, n_code,
-                glob_filter = self.cfg.src_globs,
-                file_mode = 'xml')
+                glob_filter = self.cfg.src_globs)
 
     @step
     def llm_repair(self, n_code: TreeNode, n_op_test: TestResultNode) -> TreeNode:
@@ -405,7 +404,6 @@ class Workflow:
             n_op_test: TestResultNode) -> tuple[TreeNode, LlmOpNode]:
         return llm.run_rewrite(
                 self.cfg, self.mvir, LLM_REPAIR_PROMPT, n_code,
-                file_mode = 'xml',
                 glob_filter = self.cfg.src_globs,
                 format_kwargs = {'test_output': n_op_test.body_str()},
                 think = True)
@@ -431,7 +429,6 @@ class Workflow:
             for j in json_errors if j.get('reason') == 'compiler-message')
         return llm.run_rewrite(
                 self.cfg, self.mvir, LLM_REPAIR_COMPILE_PROMPT, n_code,
-                file_mode = 'xml',
                 glob_filter = self.cfg.src_globs,
                 format_kwargs = {'stderr': stderr},
                 think = True)

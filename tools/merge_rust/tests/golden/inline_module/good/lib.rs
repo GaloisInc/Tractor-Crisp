5dce2f4a7cf88dec31903c340e65d0adb9d07882
Stuart Pernsteiner <spernsteiner@galois.com>
2026-02-18 16:20:31 -0800

    merge_rust: fix handling of rewrites inside inline modules

---
+++ tools/merge_rust/tests/golden/inline_module/good/lib.rs
@@ -0,0 +1,19 @@
+pub fn f() -> i32 {
+    10
+}
+
+mod foo {
+    pub fn g() -> i32 {
+        20
+    }
+
+    mod bar;
+
+
+pub fn g2() -> i32 {
+        20
+    }}
+
+pub fn f2() -> i32 {
+    10
+}

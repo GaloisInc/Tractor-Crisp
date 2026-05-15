a853988165f051ad12a9da1febd2698fb6bd6bb3
Stuart Pernsteiner <spernsteiner@galois.com>
2026-05-15 11:42:22 -0700

    find_unsafe2: remove unused `extern crate`s

--- tools/find_unsafe2/src/lib.rs
+++ tools/find_unsafe2/src/lib.rs
@@ -1,10 +1,6 @@
 #![feature(rustc_private)]
-extern crate rustc_public;
-
-// Required by rustc_public::run! macro
-extern crate rustc_driver;
-extern crate rustc_interface;
 extern crate rustc_middle;
+extern crate rustc_public;
 
 use std::collections::HashMap;
 use indexmap::IndexMap;

603ab63d84d899f2c7199680f0b1748229ebe4e4
Sourya Dey <sourya@galois.com>
2026-09-04 10:17:30 -0700

    rework functions (and tests) for formatted blocks

--- crisp/gepa_common.py
+++ crisp/gepa_common.py
@@ -1,5 +1,6 @@
 """Common code for GEPA prompt optimization."""
 
+import dataclasses
 from pathlib import Path
 import re
 
@@ -42,7 +43,7 @@ def get_workflow_for_project(project_folder: Path) -> Workflow:
     return workflow
 
 
-def get_expected_formatted_blocks_from_seed_candidate(seed_candidate: dict[str, str]) -> dict[str, set[str]]:
+def get_expected_formatted_blocks(seed_candidate: dict[str, str]) -> dict[str, set[str]]:
     """
     Get the expected formatted blocks from all prompts in a seed candidate.
 
@@ -64,42 +65,63 @@ def get_expected_formatted_blocks_from_seed_candidate(seed_candidate: dict[str, 
     expected_formatted_blocks = {}
     for prompt_type, prompt in seed_candidate.items():
         expected_formatted_blocks[prompt_type] = set(re.findall(r'\{[^{}]+\}', prompt))
-    return expected_formatted_blocks
-
-
-def get_bad_prompts_in_candidate(
-    candidate: dict[str, str],
-    expected_formatted_blocks: dict[str, set[str]]
-) -> set[str]:
-    """
-    Check if all prompts in a candidate have the correct formatted blocks.
-
-    Inputs:
-    - candidate: The candidate to be evaluated. E.g.:
-    - expected_formatted_blocks: The expected formatted blocks in each prompt. Keys must match those in `candidate`. E.g.:
-
-    Example:
-        ```
-        candidate = {
-            'prompt1': "hello how is {name}",
-            'prompt2': "here's the number: {number}. Now, follow {instructions}."
-        }
-        expected_formatted_blocks = {
-            'prompt1': {'{name}'},
-            'prompt2': {'{number}', '{id}', '{instructions}'}
-        }
-        ```
-        In this case, 'prompt2' doesn't have the expected formatted blocks. Thus, {'prompt2'} will be returned.
-    """
-    assert candidate.keys() == expected_formatted_blocks.keys()
     assert all(
         (block.startswith('{') and block.endswith('}'))
         for expected_blocks in expected_formatted_blocks.values()
         for block in expected_blocks
     )
+    return expected_formatted_blocks
 
-    bad_prompt_types = set()
+
+@dataclasses.dataclass(frozen=True)
+class FBlockReport:
+    """Report on mismatch in formatted blocks between a candidate prompt and the expectation."""
+
+    missing_expected_fblocks: set[str] = dataclasses.field(default_factory=set)
+    """Formatted blocks which the candidate is expected to have, but doesn't."""
+
+    has_extra_fblocks: set[str] = dataclasses.field(default_factory=set)
+    """Formatted blocks which the candidate has, but were not expected."""
+
+
+def analyze_formatted_blocks_in_candidate(
+    candidate: dict[str, str],
+    expected_formatted_blocks: dict[str, set[str]]
+) -> dict[str, FBlockReport]:
+    """
+    Given a candidate with prompts and the expected formatted blocks for each prompt, return the FBlockReport for each prompt.
+
+    Example:
+        ```
+        process_formatted_blocks_in_candidate(
+            candidate = {
+                'prompt1': "Hello how is {name}? Good to see {person}.",
+                'prompt2': "Here's the number: {number}. Now, follow {instructions}. Remember to call {abc} and {xyz}, okay?"
+            }
+            expected_formatted_blocks = {
+                'prompt1': {'{name}'},
+                'prompt2': {'{number}', '{id}', '{instructions}'}
+            }
+        )
+        will return
+        {
+            'prompt1': FBlockReport(
+                missing_expected_fblocks = set(),
+                has_extra_fblocks = {'{person}'}
+            ),
+            'prompt2': FBlockReport(
+                missing_expected_fblocks = {'{id}'},
+                has_extra_fblocks = {'{abc}', '{xyz}'}
+            )
+        }
+        ```
+    """
+    assert candidate.keys() == expected_formatted_blocks.keys()
+    result = {}
     for prompt_type, current_expected_formatted_blocks in expected_formatted_blocks.items():
-        if set(re.findall(r'\{[^{}]+\}', candidate[prompt_type])) != current_expected_formatted_blocks:
-            bad_prompt_types.add(prompt_type)
-    return bad_prompt_types
+        candidate_formatted_blocks = set(re.findall(r'\{[^{}]+\}', candidate[prompt_type]))
+        result[prompt_type] = FBlockReport(
+            missing_expected_fblocks = current_expected_formatted_blocks - candidate_formatted_blocks,
+            has_extra_fblocks = candidate_formatted_blocks - current_expected_formatted_blocks
+        )
+    return result
--- tests/test_gepa.py
+++ tests/test_gepa.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from crisp import gepa_common as mut
 
 
-def test_get_expected_formatted_blocks_from_seed_candidate():
+def test_get_expected_formatted_blocks():
     seed_candidate = {
         'agent_safety_prompt': (Path(__file__).resolve().parent.parent / 'gepa_artifacts/test/agent_safety_prompt.txt').read_text(),
 
@@ -11,7 +11,7 @@ def test_get_expected_formatted_blocks_from_seed_candidate():
 
         'some_prompt': "here's the number:{number}. Now, follow {instructions} as per your ID: A-{id}B"
     }
-    assert mut.get_expected_formatted_blocks_from_seed_candidate(
+    assert mut.get_expected_formatted_blocks(
         seed_candidate = seed_candidate
     ) == {
         'agent_safety_prompt': {'{target_goal}', '{after_refactoring_instruction}', '{cargo_dir_path}'},
@@ -20,7 +20,7 @@ def test_get_expected_formatted_blocks_from_seed_candidate():
     }
 
 
-def test_get_bad_prompts_in_candidate():
+def test_analyze_formatted_blocks_in_candidate():
     expected_formatted_blocks = {
         'agent_safety_prompt': {'{target_goal}', '{after_refactoring_instruction}', '{cargo_dir_path}'},
         'agent_target_goal_field': {'{field_name}', '{struct_name}'},
@@ -34,22 +34,39 @@ def test_get_bad_prompts_in_candidate():
 
         'some_prompt': "here's the number:{number}. Now, follow {instructions} as per your ID: A-{id}{B}"
     }
-    assert mut.get_bad_prompts_in_candidate(
+    assert mut.analyze_formatted_blocks_in_candidate(
         candidate = candidate1,
         expected_formatted_blocks = expected_formatted_blocks
-    ) == {'agent_target_goal_field', 'some_prompt'}
+    ) == {
+        'agent_safety_prompt': mut.FBlockReport(),
+        'agent_target_goal_field': mut.FBlockReport(
+            missing_expected_fblocks = {'{struct_name}'}
+        ),
+        'some_prompt': mut.FBlockReport(
+            has_extra_fblocks = {'{B}'}
+        )
+    }
 
     candidate2 = {
         'agent_safety_prompt': (Path(__file__).resolve().parent.parent / 'gepa_artifacts/test/agent_safety_prompt_bad.txt').read_text(),
 
-        'agent_target_goal_field': "Your current target is the `{field_name}` field of `{struct_name}`, along with any similar or closely related fields.  Your goal is to change the field to use a safe type (such as `Box`, `Vec`, or `Rc`) instead of a raw pointer, and to update all uses of it to be as safe as possible (for example, use sites should not cast back to a raw pointer, since that defeats some of the safety checking).".strip(),
+        'agent_target_goal_field': "Your current target is the `{field_name}` field of `{struct_name}`, along with any similar or closely related fields.  Your goal is to change the field to use a safe type (such as `Box`, `Vec`, or `Rc`) instead of a {raw pointer}, and to update all uses of it to be as safe as possible (for example, use sites should not cast back to a raw pointer, since that defeats some of the safety checking).".strip(),
 
         'some_prompt': "here's the number:{number}. Now, follow {instructions} as per your ID: A-{id}B"
     }
-    assert mut.get_bad_prompts_in_candidate(
+    assert mut.analyze_formatted_blocks_in_candidate(
         candidate = candidate2,
         expected_formatted_blocks = expected_formatted_blocks
-    ) == {'agent_safety_prompt'}
+    ) == {
+        'agent_safety_prompt': mut.FBlockReport(
+            missing_expected_fblocks = {'{cargo_dir_path}'},
+            has_extra_fblocks = {'{cargo_path}', '{\n    func foo:\n        haha\n}'}
+        ),
+        'agent_target_goal_field': mut.FBlockReport(
+            has_extra_fblocks = {'{raw pointer}'}
+        ),
+        'some_prompt': mut.FBlockReport()
+    }
 
     candidate3 = {
         'agent_safety_prompt': (Path(__file__).resolve().parent.parent / 'gepa_artifacts/test/agent_safety_prompt_fine.txt').read_text(),
@@ -58,7 +75,17 @@ def test_get_bad_prompts_in_candidate():
 
         'some_prompt': "the quick brown fox jumps over the \n\n\n{id}\nand then goes to find{number} of" + "{instructions}"
     }
-    assert mut.get_bad_prompts_in_candidate(
+    assert mut.analyze_formatted_blocks_in_candidate(
         candidate = candidate3,
         expected_formatted_blocks = expected_formatted_blocks
-    ) == set()
+    ) == {
+        'agent_safety_prompt': mut.FBlockReport(),
+        'agent_target_goal_field': mut.FBlockReport(),
+        'some_prompt': mut.FBlockReport()
+    }
+
+
+if __name__ == "__main__":
+    # since we are not using pytest (and unittest is cumbersome to use compared to pytest), calling these through main:
+    test_get_expected_formatted_blocks()
+    test_analyze_formatted_blocks_in_candidate()

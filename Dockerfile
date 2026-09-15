9589f4b6b397e8fcf13f62ece9e8c6a6fe561c0b
Stuart Pernsteiner <spernsteiner@galois.com>
2026-09-14 17:04:01 -0700

    add scripts for calling the CRISP HTTP API

--- Dockerfile
+++ Dockerfile
@@ -118,6 +118,10 @@ RUN workdir="$(mktemp -d)" \
 # from /etc/profile and Codex uses `bash -lc` a lot
 RUN echo "export PATH=$PATH:${CARGO_HOME}/bin" >>/etc/profile
 
+# Copy helper scripts for calling the CRISP internal HTTP API.
+COPY scripts/crisp_check_unsafe2.sh /usr/local/bin/
+COPY scripts/crisp_run_tests.sh /usr/local/bin/
+
 
 FROM tractor-crisp-user AS tractor-crisp
 
---
+++ scripts/crisp_run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# Ask the CRISP harness to run the test suite.  CRISP will extract a snapshot
+# of the translated Rust code from the current sandbox, run the tests on it,
+# and output the results.  Note that this always uses the original version of
+# the tests; changes to the test code are not allowed and will be ignored.
+# This script exits 0 if all tests pass or nonzero if any of them fail.
+
+curl -X POST --fail-with-body \
+    http://$CRISP_INTERNAL_API_HOST:$CRISP_INTERNAL_API_PORT/crisp/run_tests
